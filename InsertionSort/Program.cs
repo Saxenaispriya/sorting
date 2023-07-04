5f4e0e16fca2271f379003fcb2309bc67d8b03ec
Priyanka saxena <saxena23priya@gmail.com>
2023-07-04 23:14:53 +0530

    new commit040723

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {5,4,10,1,6,2};
+
+            for(int i=1;i<6;i++)
+            {
+                int temp = arr[i];
+                int j = i - 1;
+                while(j>=0 && temp < arr[j])
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = arr[j];
+            }
+
+            for(int i=0;i<6;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {7,4,10,8,3,1 };
+            for(int i=0;i<6-1;i++)
+            {
+                int min = i;
+                for(int j=i+1;j<6;j++)
+                {
+                    if (arr[min] > arr[j])
+                    {
+                        min= j;
+                    }
+                }
+                if(min!=i)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[min];
+                    arr[min] = temp;
+                }
+            }
+
+            for(int i=0;i<6;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
