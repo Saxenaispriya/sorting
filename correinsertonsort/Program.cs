aa61204303375f2da5c42b3a4d70f0cf3154cf4b
Priyanka saxena <saxena23priya@gmail.com>
2023-07-05 08:10:21 +0530

    correction selection sort commit05mor

---
+++ correinsertonsort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace correinsertonsort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {10,5,0,2,7,9 };
+
+            for(int i=0;i<6;i++)
+            {
+                for(int j=i+1;j<6;j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            for(int i=0;i<6;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
